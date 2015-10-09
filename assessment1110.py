f51f0143b11087c4a0d4f7cea10b59da63a25e65
unknown <mgorman@yak.cartongroup.lan>
2015-10-09 22:54:54 +0100

    hard work but getting there.

--- assessment1110.py
+++ assessment1110.py
@@ -12,12 +12,31 @@ c = conn.cursor()
 #print each item in the table "location"
 c.execute("SELECT * FROM location;")
 print ("Locations are: ")
-for item in c:
-    print item
+l = list(c.execute("SELECT * FROM location"))
+print l
+#for i in l:
+#    print i
 
 #print each item in the table "ports"
 c.execute("SELECT * FROM ports;")
 print "Ports are: "
-for item in c:
-    print item
+p = list(c.execute("SELECT * FROM ports"))
+print p
+#for j in p:
+#   print j
 
+
+print " "
+p0 = p[0]
+print p0[0]
+print p0[1]
+print p0[0] - p0[1]
+#print p[1]
+#print p[2]
+#print p[-1]
+#print p[3]
+#print l[0]
+
+
+
+#print min(items[1],item[1])
